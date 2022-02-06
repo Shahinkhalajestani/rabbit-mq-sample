2abd03a508f98ae7f411815e5a2160b6c525518f
Shahinkhalajestani <vatharishok@gmail.com>
2022-02-06 15:23:46 +0330

    First Commit

---
+++ src/test/java/com/shahintraining/rabbitmqspringboot/RabbitMqSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shahintraining.rabbitmqspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitMqSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
